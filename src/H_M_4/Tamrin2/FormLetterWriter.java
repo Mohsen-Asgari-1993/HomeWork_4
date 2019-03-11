50b0f3cf3e8a3866d69a43b39beb5d94cee711f4
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 14:23:10 +0330

    Adds main method and call both displaySalutation method with different params

--- src/H_M_4/Tamrin2/FormLetterWriter.java
+++ src/H_M_4/Tamrin2/FormLetterWriter.java
@@ -1,11 +1,18 @@
 package H_M_4.Tamrin2;
 
 public class FormLetterWriter {
+    public static void main(String[] args) {
+        String firstName = "mohsen";
+        String lastName = "Asgari";
+        displaySalutation(lastName);
+
+        displaySalutation(firstName,lastName);
+    }
 
     public static void displaySalutation(String lastname){
-        System.out.print("Dear Me."+lastname+" Thank you for your recent order");
+        System.out.println("Dear Mr."+lastname+"\n Thank you for your recent order");
     }
     static void displaySalutation(String firstName , String lastName){
-        System.out.print("Dear Me."+firstName+" "+lastName+" Thank you for your recent order");
+        System.out.println("Dear Mr."+firstName+" "+lastName+"\n Thank you for your recent order");
     }
 }
