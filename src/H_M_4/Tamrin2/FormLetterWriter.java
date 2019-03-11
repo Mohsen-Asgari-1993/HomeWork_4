740735f1427eafc64e6724267f6ad6d2e73dc4b0
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 14:19:33 +0330

    Adds displaySalutation with one String param

--- src/H_M_4/Tamrin2/FormLetterWriter.java
+++ src/H_M_4/Tamrin2/FormLetterWriter.java
@@ -1,4 +1,9 @@
 package H_M_4.Tamrin2;
 
 public class FormLetterWriter {
+
+    public static void displaySalutation(String lastname){
+        System.out.print("Dear Me."+lastname+" Thank you for your recent order");
+    }
+    
 }
