05526a898d5c4c9e2475ad169b7fadec44096250
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 13:00:00 +0330

    fixex encrypt method and adds encryptAll method

--- src/H_M_4/Encryption.java
+++ src/H_M_4/Encryption.java
@@ -12,7 +12,24 @@ public class Encryption {
         this.code = code;
     }
 
-    public char encrypt(char ch, int code) {
-        return (char) (ch + code);
+    public char encrypt(char ch) {
+        int ascii = ch;
+        return (char) (ascii + code);
+    }
+
+    public String encryptAll() {
+        String encrypted="";
+        int length = message.length();
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            if (message.charAt(i) == ' ') {
+                chars[i] = ' ';
+            } else {
+                chars[i] =encrypt(message.charAt(i));
+            }
+        }
+        for (char c : chars)
+            encrypted +=c;
+        return encrypted;
     }
 }
