0f3e01c7543f0cd49431ca8a06de5bad25d21c17
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 13:55:41 +0330

    Adds Scanner class , get message and code from user and call encryptAll

--- src/H_M_4/EncryptMessage.java
+++ src/H_M_4/EncryptMessage.java
@@ -1,4 +1,14 @@
 package H_M_4;
 
+import java.util.Scanner;
+
 public class EncryptMessage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your message : ");
+        String message = scanner.nextLine();
+        System.out.println("please enter your code : ");
+        int code = scanner.nextInt();
+        System.out.println(Encryption.encryptAll(message,code));
+    }
 }
