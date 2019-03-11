6dbf8efbe51633c6c30f5389e90c560b96c76517
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 13:12:29 +0330

    Adds scanner , get massage and code from user and call encryptAll method

--- src/H_M_4/EncryptMessage.java
+++ src/H_M_4/EncryptMessage.java
@@ -1,8 +1,19 @@
 package H_M_4;
 
 
-public class EncryptMessage {
+import java.util.Scanner;
 
+public class EncryptMessage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter your message : ");
+        String message = scanner.nextLine();
+        System.out.println("please enter your code : ");
+        int code = scanner.nextInt();
+        Encryption encryption = new Encryption();
+        encryption.getCode(code);
+        encryption.getMessage(message);
+        System.out.println(encryption.encryptAll());
     }
 }
 
