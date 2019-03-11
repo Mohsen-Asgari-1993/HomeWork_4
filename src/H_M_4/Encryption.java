455a9176a0274dc994fcd4603dad2af96de8da22
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 13:49:34 +0330

    Adds public static encryptAll method

--- src/H_M_4/Encryption.java
+++ src/H_M_4/Encryption.java
@@ -6,5 +6,19 @@ public class Encryption {
         return (char) (ascii + code);
     }
 
-
+    public static String encryptAll(String message, int code) {
+        int length = message.length();
+        char[] chars = new char[length];
+        for (int i = 0; i < length; i++) {
+            if(message.charAt(i)==' ')
+                chars[i]=' ';
+            else {
+                chars[i]=encrypt(message.charAt(i),code);
+            }
+        }
+        String encrypted="";
+        for (char ch : chars)
+            encrypted +=ch;
+        return encrypted;
+    }
 }
