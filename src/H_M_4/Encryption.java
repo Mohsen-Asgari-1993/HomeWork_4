d3c20e258b2a10ae6ee12b0827c4a50bf0a993e5
mohsen <mohsen1993asgari@gmail.com>
2019-03-11 12:24:32 +0330

    Adds Encrypt Method

--- src/H_M_4/Encryption.java
+++ src/H_M_4/Encryption.java
@@ -3,10 +3,16 @@ package H_M_4;
 public class Encryption {
     private String message;
     private int code;
-    public void getMessage(String massage){
-        this.message=massage;
+
+    public void getMessage(String massage) {
+        this.message = massage;
     }
-    public void getCode(int code){
-        this.code=code;
+
+    public void getCode(int code) {
+        this.code = code;
+    }
+
+    public char encrypt(char ch, int code) {
+        return (char) (ch + code);
     }
 }
