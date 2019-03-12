8e51d04e5eac85ea13e068787da6048e87a601b8
mohsen <mohsen1993asgari@gmail.com>
2019-03-12 13:01:33 +0330

    Adds get methods

--- src/H_M_4/Tamrin4/Patient.java
+++ src/H_M_4/Tamrin4/Patient.java
@@ -4,4 +4,16 @@ public class Patient {
     private int id;
     private int age;
     private BloodData bloodData;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public BloodData getBloodData() {
+        return bloodData;
+    }
 }
