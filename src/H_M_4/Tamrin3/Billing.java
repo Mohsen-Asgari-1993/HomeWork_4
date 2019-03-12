5fb55332e51a84f4ebaa200d877c4e84839a14d4
mohsen <mohsen1993asgari@gmail.com>
2019-03-12 09:39:26 +0330

    Adds main method and tests computeBill's method

--- src/H_M_4/Tamrin3/Billing.java
+++ src/H_M_4/Tamrin3/Billing.java
@@ -1,6 +1,16 @@
 package H_M_4.Tamrin3;
 
 public class Billing {
+    public static void main(String[] args) {
+        double price = 17850.5;
+        int quantity = 5;
+        double coupon = 14500;
+        System.out.println(computeBill(price));
+        System.out.println(computeBill(price,quantity));
+        System.out.println(computeBill(price,quantity,coupon));
+
+    }
+
     public static double computeBill(double price) {
         double tax = price + (0.08 * price);
         return tax;
