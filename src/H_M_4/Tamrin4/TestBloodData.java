a18d2e76ad8646dac52978db19ac0841594e38a1
mohsen <mohsen1993asgari@gmail.com>
2019-03-12 11:00:37 +0330

    Creates two object for testing BloodData

--- src/H_M_4/Tamrin4/TestBloodData.java
+++ src/H_M_4/Tamrin4/TestBloodData.java
@@ -2,6 +2,11 @@ package H_M_4.Tamrin4;
 
 public class TestBloodData {
     public static void main(String[] args) {
-        
+        BloodData bloodData = new BloodData();
+        System.out.println(bloodData.getBloodType());
+        System.out.println(bloodData.getRh());
+        BloodData bloodData1 = new BloodData("A", '+');
+        System.out.println(bloodData1.getBloodType());
+        System.out.println(bloodData1.getRh());
     }
 }
