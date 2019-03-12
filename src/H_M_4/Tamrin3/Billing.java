416d8ddc00cc7693b0d52b4bcfe779e43d61d725
mohsen <mohsen1993asgari@gmail.com>
2019-03-12 09:35:58 +0330

    Adds computeBill method with three parameters

--- src/H_M_4/Tamrin3/Billing.java
+++ src/H_M_4/Tamrin3/Billing.java
@@ -10,4 +10,14 @@ public class Billing {
         double tax = price + (0.08 * price);
         return tax * quantity;
     }
+
+    public static double computeBill(double price, int quantity, double coupon) {
+        double totalPrice = (price * quantity) - coupon;
+        if (totalPrice <= 0)
+            return 0;
+        else
+            return totalPrice + (0.08 * totalPrice);
+
+    }
 }
+
